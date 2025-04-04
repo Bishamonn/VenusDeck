f20a910ddc6beaff3fb68b80e012321f4acbce5d
Bishamonn <157050349+Bishamonn@users.noreply.github.com>
2025-04-04 16:50:10 +0300

    Bozuk
    ekranı 4'e 2x2 bölmeye çalıştım ama x değeri hep her şekilde sabit kalıyor ve bozuk oluyor amk

--- deniyorum/oluyo/oluyo.ino
+++ deniyorum/oluyo/oluyo.ino
@@ -9,7 +9,7 @@ TFT_eSPI tft = TFT_eSPI();
 
 // Dokunmatik ekran pinleri
 const int XP = 23, XM = 32, YP = 21, YM = 13;
-const int TS_LEFT = -2500, TS_RT = 517, TS_TOP = 780, TS_BOT = -2650;
+const int TS_LEFT = 20, TS_RT = 466, TS_TOP = -53, TS_BOT = 373;
 TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
 
 int pixel_x, pixel_y;
@@ -26,39 +26,59 @@ bool Touch_getXY() {
   digitalWrite(XM, HIGH);
   p.z = abs(p.z);
   bool pressed = (p.z > MINPRESSURE && p.z < MAXPRESSURE);
+
   if (pressed) {
-    pixel_x = map(p.x, TS_LEFT, TS_RT, 0, tft.width());
-    pixel_y = map(p.y, TS_TOP, TS_BOT, 0, tft.height());
+    // **X ve Y eksenlerini değiştirdik!**
+    int temp_x = map(p.x, TS_LEFT, TS_RT, 0, tft.width());
+    int temp_y = map(p.y, TS_TOP, TS_BOT, 0, tft.height());
+
+    // **Negatif Y değerlerini düzeltme**
+    if (temp_y < 0) {
+      temp_y = abs(temp_y);
+    }
+
+    pixel_x = temp_x;
+    pixel_y = temp_y;
+
+    Serial.print("Düzgün Dokunma X: ");
+    Serial.print(pixel_x);
+    Serial.print(" Y: ");
+    Serial.println(pixel_y);
   }
+
   return pressed;
 }
 
 void setup() {
-  Serial.begin(921600);
+  Serial.begin(115200);
   tft.init();
   tft.setRotation(1);
 
-  // **Ekranı beşe böl ve farklı renklerde göster**
+  // **Ekranı 2x2 bölümlere ayır ve renklendir**
   tft.fillScreen(TFT_BLACK);
-  tft.fillRect(0, 0, tft.width(), tft.height() / 5, TFT_GREEN);    // Ses Aç
-  tft.fillRect(0, tft.height() / 5, tft.width(), tft.height() / 5, TFT_RED);  // Ses Kıs
-  tft.fillRect(0, 2 * tft.height() / 5, tft.width(), tft.height() / 5, TFT_YELLOW);  // Sonraki Şarkı
-  tft.fillRect(0, 3 * tft.height() / 5, tft.width(), tft.height() / 5, TFT_ORANGE);  // Önceki Şarkı
-  tft.fillRect(0, 4 * tft.height() / 5, tft.width(), tft.height() / 5, TFT_BLUE);  // Spotify Aç
+  int w = tft.width() / 2;
+  int h = tft.height() / 2;
 
-  // **Bilgilendirici yazılar**
+  tft.fillRect(0, 0, w, h, TFT_GREEN);    // Ses Aç
+  tft.fillRect(w, 0, w, h, TFT_RED);      // Ses Kıs
+  tft.fillRect(0, h, w, h, TFT_YELLOW);   // Önceki Şarkı
+  tft.fillRect(w, h, w, h, TFT_BLUE);     // Sonraki Şarkı
+
+  // **Bilgilendirici yazılar (Ortalanmış)**
   tft.setTextColor(TFT_WHITE);
   tft.setTextSize(2);
-  tft.setCursor(50, tft.height() / 10);
-  tft.println("BASLA / DUR");
-  tft.setCursor(50, 3 * tft.height() / 10);
-  tft.println("ONCEKİ SARKI");
-  tft.setCursor(50, 5 * tft.height() / 10);
-  tft.println("SONRAKI SARKI");
-  tft.setCursor(50, 7 * tft.height() / 10);
+
+  tft.setCursor(w / 4, h / 3);
   tft.println("SES AC");
-  tft.setCursor(50, 9 * tft.height() / 10);
+
+  tft.setCursor(3 * w / 4 - 30, h / 3);
   tft.println("SES KIS");
+
+  tft.setCursor(w / 4, h + h / 3);
+  tft.println("ONCEKI");
+
+  tft.setCursor(3 * w / 4 - 30, h + h / 3);
+  tft.println("SONRAKI");
 
   Serial.println("BLE Klavye başlatılıyor...");
   bleKeyboard.begin();
@@ -67,23 +87,24 @@ void setup() {
 void loop() {
   if (bleKeyboard.isConnected()) {
     if (Touch_getXY()) {
-      if (pixel_y < tft.height() / 5) {
-        bleKeyboard.write(KEY_MEDIA_VOLUME_DOWN);
-        Serial.println("Ses Kısma Tuşu Gönderildi!");
-      } else if (pixel_y < 2 * tft.height() / 5) {
+      int w = tft.width() / 2;
+      int h = tft.height() / 2;
+
+      if (pixel_x < w && pixel_y < h) {
         bleKeyboard.write(KEY_MEDIA_VOLUME_UP);
-        Serial.println("Ses Açma Tuşu Gönderildi!");
-      } else if (pixel_y < 3 * tft.height() / 5) {
-        bleKeyboard.write(KEY_MEDIA_NEXT_TRACK);
-        Serial.println("Sonraki Şarkı!");
-      } else if (pixel_y < 4 * tft.height() / 5) {
-       bleKeyboard.write(KEY_MEDIA_PREVIOUS_TRACK);
+        Serial.println("Ses Aç!");
+      } else if (pixel_x >= w && pixel_y < h) {
+        bleKeyboard.write(KEY_MEDIA_VOLUME_DOWN);
+        Serial.println("Ses Kıs!");
+      } else if (pixel_x < w && pixel_y >= h) {
+        bleKeyboard.write(KEY_MEDIA_PREVIOUS_TRACK);
         Serial.println("Önceki Şarkı!");
       } else {
-       bleKeyboard.write(KEY_MEDIA_PLAY_PAUSE);
-        Serial.println("Dur, Başla!");
+        bleKeyboard.write(KEY_MEDIA_NEXT_TRACK);
+        Serial.println("Sonraki Şarkı!");
       }
-      delay(10);
+
+      delay(100);
       while (Touch_getXY()) {
         delay(50);
       }
