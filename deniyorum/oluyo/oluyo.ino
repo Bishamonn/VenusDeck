8facd91b021fd4a44fc8920ac51bbe340413249a
Bishamonn <157050349+Bishamonn@users.noreply.github.com>
2025-04-06 15:14:27 +0300

    06.04.2025  XXXX
    bu kodda bluetooth bağlantısını başarıyla yaptım. ama masaüstü uygulaması entegre etmeye çalışırken biraz bozuldu. o yüzden kullanma

--- deniyorum/oluyo/oluyo.ino
+++ deniyorum/oluyo/oluyo.ino
@@ -7,20 +7,17 @@
 #define GREEN   0x07E0
 #define BLUE    0x001F
 #define WHITE   0xFFFF
-#define GRAY    0x8410
 #define YELLOW  0xFFE0
 #define CYAN    0x07FF
 #define MAGENTA 0xF81F
 #define ORANGE  0xFD20
 #define PURPLE  0x8010
 
-// Dokunmatik kalibrasyon
 #define TS_MINX 150
 #define TS_MAXX 920
 #define TS_MINY 120
 #define TS_MAXY 940
 
-// Dokunmatik pinler
 #define XP 9
 #define XM A3
 #define YP A2
@@ -31,71 +28,79 @@
 MCUFRIEND_kbv tft(A3, A2, A1, A0, A4);
 TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
 
-// 4x2 düzeninde 8 buton için ayarlar
 #define BUTTON_ROWS 4
 #define BUTTON_COLUMNS 2
 
-// Buton durumları
-bool buttonPressed[8] = {false, false, false, false, false, false, false, false};
-bool lastButtonState[8] = {false, false, false, false, false, false, false, false};
+bool buttonPressed[8] = {false};
+bool lastButtonState[8] = {false};
 
-// Buton renkleri (normal durum)
-uint16_t buttonColors[8] = {RED, GREEN, BLUE, YELLOW, CYAN, MAGENTA, ORANGE, PURPLE};
+uint16_t buttonColors[8] = {
+  RED, GREEN, BLUE, YELLOW,
+  CYAN, MAGENTA, ORANGE, PURPLE
+};
 
-// Buton x ve y pozisyonları, genişlikleri ve yükseklikleri
+const char* buttonCommands[8] = {
+  "CMD: VOLUME_DOWN",
+  "CMD: VOLUME_UP",
+  "CMD: BRIGHTNESS_DOWN",
+  "CMD: BRIGHTNESS_UP",
+  "CMD: OPEN_BROWSER",
+  "CMD: CLOSE_APP",
+  "CMD: LOCK_SCREEN",
+  "CMD: ALT_TAB"
+};
+
+const char* buttonLabels[8] = {
+  "SES -", "SES +", "PARLAK -", "PARLAK +",
+  "TARAYICI", "KAPAT", "KILITLE", "ALT+TAB"
+};
+
 int buttonX[8];
 int buttonY[8];
 int buttonWidth;
 int buttonHeight;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600);      // Bilgisayar üzerinden debug için
+  Serial1.begin(9600);     // HC-05 ile haberleşme
+
   uint16_t ID = tft.readID();
   tft.begin(ID);
-  tft.setRotation(0); // Dikey mod
+  tft.setRotation(0);
   tft.fillScreen(BLACK);
-  
-  // Ekran boyutlarına göre buton boyutlarını hesapla
+
   buttonWidth = tft.width() / BUTTON_COLUMNS;
   buttonHeight = tft.height() / BUTTON_ROWS;
-  
-  // Buton konumlarını hesapla ve butonları çiz
+
   for (int i = 0; i < 8; i++) {
     int row = i / BUTTON_COLUMNS;
     int col = i % BUTTON_COLUMNS;
-    
+
     buttonX[i] = col * buttonWidth;
     buttonY[i] = row * buttonHeight;
-    
+
     drawButton(i, false);
   }
-  
-  Serial.println("8 buton tam ekran olarak hazır. Dokunmatik test başladı.");
+
+  Serial.println("Dokunmatik kontrolcü hazır.");
 }
 
 void loop() {
   TSPoint p = ts.getPoint();
-  
-  // Dokunmatik pinleri geri ayarla
+
   pinMode(YP, OUTPUT);
   pinMode(XM, OUTPUT);
   digitalWrite(YP, HIGH);
   digitalWrite(XM, HIGH);
-  
-  // Tüm butonları basılmamış duruma getir
+
   for (int i = 0; i < 8; i++) {
     buttonPressed[i] = false;
   }
-  
-  // Dokunma kontrolü
+
   if (p.z > TS_PRESSURE_MIN && p.z < TS_PRESSURE_MAX) {
     int x = map(p.x, TS_MINX, TS_MAXX, 0, tft.width());
-    int y = map(p.y, TS_MINY, TS_MAXY, tft.height(), 0)+10;
-    
-    Serial.print("Dokunulan: x="); Serial.print(x);
-    Serial.print(" y="); Serial.println(y);
-    
-    // Tüm butonları kontrol et
+    int y = map(p.y, TS_MINY, TS_MAXY, tft.height(), 0) + 10;
+
     for (int i = 0; i < 8; i++) {
       if (x >= buttonX[i] && x < buttonX[i] + buttonWidth &&
           y >= buttonY[i] && y < buttonY[i] + buttonHeight) {
@@ -103,52 +108,40 @@ void loop() {
       }
     }
   }
-  
-  // Buton durumlarını güncelle
+
   for (int i = 0; i < 8; i++) {
     if (buttonPressed[i] != lastButtonState[i]) {
       drawButton(i, buttonPressed[i]);
       lastButtonState[i] = buttonPressed[i];
-      
+
       if (buttonPressed[i]) {
-        Serial.print("Buton ");
-        Serial.print(i + 1);
-        Serial.println(" basıldı!");
-        // Her buton için özel işlevler eklenebilir
+        Serial1.println(buttonCommands[i]); // Sadece HC-05'e gönder
+        Serial.println(buttonCommands[i]);  // Debug
       }
     }
   }
-  
-  delay(50); // Kararlı çalışma için küçük bir gecikme
+
+  delay(50);
 }
 
-// Belirtilen indeksteki butonu çizen fonksiyon
-void drawButton(int buttonIndex, bool pressed) {
-  uint16_t fill_color, text_color, outline_color;
-  
-  if (pressed) {
-    fill_color = WHITE;
-    text_color = BLACK;
-    outline_color = BLACK;
-  } else {
-    fill_color = buttonColors[buttonIndex];
-    text_color = WHITE;
-    outline_color = WHITE;
-  }
-  
-  // Butonu çiz
-  tft.fillRect(buttonX[buttonIndex], buttonY[buttonIndex], buttonWidth, buttonHeight, fill_color);
-  tft.drawRect(buttonX[buttonIndex], buttonY[buttonIndex], buttonWidth, buttonHeight, outline_color);
-  
-  // Buton metnini ortala
+void drawButton(int i, bool pressed) {
+  uint16_t fill_color = pressed ? WHITE : buttonColors[i];
+  uint16_t text_color = pressed ? BLACK : WHITE;
+  uint16_t outline_color = WHITE;
+
+  tft.fillRect(buttonX[i], buttonY[i], buttonWidth, buttonHeight, fill_color);
+  tft.drawRect(buttonX[i], buttonY[i], buttonWidth, buttonHeight, outline_color);
+
   tft.setTextColor(text_color);
   tft.setTextSize(2);
-  
-  int textWidth = 12 * 6; // "Buton X" metni için yaklaşık genişlik
-  int x = buttonX[buttonIndex] + (buttonWidth - textWidth) / 2;
-  int y = buttonY[buttonIndex] + (buttonHeight - 16) / 2;
-  
+
+  int16_t x1, y1;
+  uint16_t w, h;
+  tft.getTextBounds(buttonLabels[i], 0, 0, &x1, &y1, &w, &h);
+
+  int x = buttonX[i] + (buttonWidth - w) / 2;
+  int y = buttonY[i] + (buttonHeight - h) / 2;
+
   tft.setCursor(x, y);
-  tft.print("Buton ");
-  tft.print(buttonIndex + 1);
+  tft.print(buttonLabels[i]);
 }
---
+++ deniyorum/sik/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.IO.Ports;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+
+
+namespace sik
+{
+    public enum CommandType
+    {
+        None,
+        VolumeUp,
+        VolumeDown,
+        BrightnessUp,
+        BrightnessDown,
+        OpenBrowser,
+        CloseApp,
+        LockScreen,
+        AltTab
+    }
+
+    public class Command
+    {
+        public string Name { get; set; }
+        public CommandType Type { get; set; }
+    }
+
+    public partial class Form1 : Form
+    {
+
+        Command[] buttonAssignments = new Command[8]; // 8 buton varsa
+        SerialPort serial;
+        int currentBrightness = 50;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            serial = new SerialPort("COM7", 9600); // HC-05 eşleştiği port
+            serial.DataReceived += Serial_DataReceived;
+
+            if (!File.Exists("buttons.json"))
+            {
+                buttonAssignments = new Command[8];
+                for (int i = 0; i < buttonAssignments.Length; i++)
+                {
+                    buttonAssignments[i] = new Command { Name = "Yok", Type = CommandType.None };
+                }
+            }
+            else
+            {
+                LoadAssignments();
+            }
+
+            try
+            {
+                serial.Open();
+                MessageBox.Show("Bluetooth bağlantısı başarılı!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bağlantı hatası: " + ex.Message);
+            }
+        }
+        private void SaveAssignments()
+        {
+            string json = JsonConvert.SerializeObject(buttonAssignments);
+            File.WriteAllText("buttons.json", json);
+        }
+
+        private void LoadAssignments()
+        {
+            if (File.Exists("buttons.json"))
+            {
+                string json = File.ReadAllText("buttons.json");
+                buttonAssignments = JsonConvert.DeserializeObject<Command[]>(json);
+            }
+        }
+
+        private void Serial_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string incoming = serial.ReadLine().Trim();
+            this.Invoke(new Action(() =>
+            {
+                HandleCommand(incoming);
+            }));
+        }
+
+        private void HandleCommand(string cmd)
+        {
+            Console.WriteLine("Gelen komut: " + cmd);
+
+            if (cmd.StartsWith("CMD:BTN"))
+            {
+                int index = int.Parse(cmd.Substring(7)); // Örn: CMD:BTN3 → 3
+                if (index >= 0 && index < buttonAssignments.Length)
+                {
+                    ExecuteCommand(buttonAssignments[index]);
+                }
+            }
+        }
+
+        private void SendKey(Keys key)
+        {
+            keybd_event((byte)key, 0, 0, 0);
+            keybd_event((byte)key, 0, 2, 0);
+        }
+
+        private void SendAltTab()
+        {
+            keybd_event((byte)Keys.Menu, 0, 0, 0);
+            keybd_event((byte)Keys.Tab, 0, 0, 0);
+            keybd_event((byte)Keys.Tab, 0, 2, 0);
+            keybd_event((byte)Keys.Menu, 0, 2, 0);
+        }
+
+        private void AdjustBrightness(int change)
+        {
+            currentBrightness = Math.Max(0, Math.Min(100, currentBrightness + change));
+            SetBrightness(currentBrightness);
+        }
+
+        private void SetBrightness(int brightness)
+        {
+            string cmd = $"(Get-WmiObject -Namespace root/WMI -Class WmiMonitorBrightnessMethods).WmiSetBrightness(1, {brightness})";
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "powershell",
+                Arguments = $"-Command \"{cmd}\"",
+                UseShellExecute = false,
+                CreateNoWindow = true
+            });
+        }
+
+        [DllImport("user32.dll")] static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
+        [DllImport("user32.dll")] static extern bool LockWorkStation();
+
+        private void Form1_Load(object sender, EventArgs e) { }
+
+        private void ExecuteCommand(Command cmd)
+        {
+            switch (cmd.Type)
+            {
+                case CommandType.VolumeUp: SendKey(Keys.VolumeUp); break;
+                case CommandType.VolumeDown: SendKey(Keys.VolumeDown); break;
+                case CommandType.BrightnessUp: AdjustBrightness(10); break;
+                case CommandType.BrightnessDown: AdjustBrightness(-10); break;
+                case CommandType.OpenBrowser: System.Diagnostics.Process.Start("https://www.google.com"); break;
+                case CommandType.CloseApp: Application.Exit(); break;
+                case CommandType.LockScreen: LockWorkStation(); break;
+                case CommandType.AltTab: SendAltTab(); break;
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveAssignments();
+        }
+    }
+
+}
